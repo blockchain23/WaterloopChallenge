33f0c335fb3af82592e78f407f82388558e6e60f
oliverwitham <39222089+oliverwitham@users.noreply.github.com>
2018-05-12 13:36:38 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,14 +1,17 @@
 #include <Wire.h>
 
 const int devAddr = 0x5A;
+const int commBytes = 32;
 
 void setup() {
   // put your setup code here, to run once:
   Wire.begin();
   Serial.begin(9600);
   Wire.beginTransmission(devAddr);
-  Wire.requestFrom(devAddr,);
-  Wire.
+  Wire.requestFrom(devAddr, commBytes);
+  for (int i = 0; i < 32; i++) {
+    Serial.println(Wire.read());
+  }
 }
 
 void loop() {
