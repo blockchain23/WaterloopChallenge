d61537118ba102eef9532dd2f7e0960ff90d03d8
oliverwitham <39222089+oliverwitham@users.noreply.github.com>
2018-05-12 15:58:21 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,11 +1,10 @@
 #include <Wire.h>
+#include <ArduinoJson.h>
 
 const int devAddr = 0x5A;
 const int commBytes = 32;
-DataPacket dataPacket;
-Pod pod;
 
-struct Pod {
+typedef struct Pod {
   long vel;
   long pos;
   long accel;
@@ -13,26 +12,29 @@ struct Pod {
   byte sendData[3];
 };
 
-struct DataPacket {
-  byte startingByte;
-  byte errorStates;
-  byte currentPodState;
-  
+typedef struct DataPacket {
+  uint8_t startingByte;
+  uint8_t errorStates;
+  uint8_t currentPodState;
+
   //Where each element is a byte for the 32 bit data.
-  byte timeStamp[4];
-  byte accelFront[4];
-  byte accelMiddle[4];
-  byte accelRear[4];
-  byte tempBraking[4];
-  byte tempPropul[4];
-  byte tempMotherboard[4];
-  byte endingByte;
+  float timeStamp;
+  float accelFront;
+  float accelMiddle;
+  float accelRear;
+  float tempBraking;
+  float tempPropul;
+  float tempMotherboard;
+  uint8_t endingByte;
 };
 
+DataPacket dataPacket;
+Pod pod;
+
 void calcPosVel() {
-  pod.accel = (dataPacket.accelFront + dataPacket.accelMid + dataPacket.accelRear)/3.0;
-  pod.pos += vel*0.002 + 0.5*accel*0.002*0.002;
-  pod.vel = vel + accel*0.002;  
+  pod.accel = (dataPacket.accelFront + dataPacket.accelMiddle + dataPacket.accelRear)/3.0;
+  pod.pos += pod.vel*0.002 + 0.5*pod.accel*0.002*0.002;
+  pod.vel = pod.vel + pod.accel*0.002;  
 }
 
 void sendToServer() {
@@ -41,41 +43,27 @@ void sendToServer() {
   root["velocity"] = pod.vel;
   root["distance"] = pod.pos;
   root["acceleration"] = pod.accel;
-  root["propulsion_temp"] = dataPacket.propulsionTemp;
-  root["braking_temp"] = dataPacket.brakingTemp;
-  root["motherboard_temp"] = dataPacket.motherboardTemp;
+  root["propulsion_temp"] = dataPacket.tempPropul;
+  root["braking_temp"] = dataPacket.tempBraking;
+  root["motherboard_temp"] = dataPacket.tempMotherboard;
   root["pod_state"] = pod.state;
-  root["time_since_start"] = dataPacket.time;
+  root["time_since_start"] = dataPacket.timeStamp;
   root.printTo(Serial);
 }
 
-DataPacket readData () {
-  uint8_t &buffer[32];
+void readData(DataPacket & dataPacket) {
+  uint8_t buffer[32];
   for (int i = 0; i < 32; i++) {
     buffer[i] = Wire.read();
   }
-  dataPacket = (DataPacket)buffer
-  return dataPacket;
+  //dataPacket = (DataPacket)buffer;
+  memcpy(&buffer,&dataPacket,sizeof(dataPacket));
+  return;
 }
 
-struct DataPacket {
-  uint8_t startingByte;
-  uint8_t errorStates;
-  uint8_t currentPodState;
 
-  //Where each element is a byte for the 32 bit data.
-  float32_t timeStamp;
-  float32_t accelFront;
-  float32_t accelMiddle;
-  float32_t accelRear;
-  float32_t tempBraking;
-  float32_t tempPropul;
-  float32_t tempMotherboard;
-  uint8_t endingByte;
-};
-
-controlPod() {
-  if (pos > (30000000-50000) && (state != 4 || state != 5)) {
+void controlPod() {
+  if (pod.pos > (30000000-50000) && (pod.state != 4 || pod.state != 5)) {
     pod.sendData[1] = 0x04;
     pod.state = 4;
     Wire.write(pod.sendData,3);
@@ -102,8 +90,8 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   
-  while (vel > 0) {
-    dataPacket = readData();
+  while (pod.vel > 0) {
+    readData(dataPacket);
     calcPosVel();
     sendToServer();
     controlPod();
