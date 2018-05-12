b9a96b8c4fced1f9521bda2d572c7b10bc7e81d3
oliverwitham <39222089+oliverwitham@users.noreply.github.com>
2018-05-12 13:34:35 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,6 +1,14 @@
+#include <Wire.h>
+
+const int devAddr = 0x5A;
+
 void setup() {
   // put your setup code here, to run once:
-
+  Wire.begin();
+  Serial.begin(9600);
+  Wire.beginTransmission(devAddr);
+  Wire.requestFrom(devAddr,);
+  Wire.
 }
 
 void loop() {
