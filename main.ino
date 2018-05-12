30821df5bc2c52a0e5933de3774bdc26a33d4656
oliverwitham <39222089+oliverwitham@users.noreply.github.com>
2018-05-12 14:30:16 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -9,15 +9,23 @@ struct pod {
   int state;
 };
 
+void calcPosVel() {
+  accel = (dataPacket.accelFront + dataPacket.accelMid + dataPacket.accelRear)/3.0;
+  pos += vel*0.002 + 0.5*accel*0.002*0.002;
+  vel = vel + accel*0.002;  
+}
+
 void setup() {
   // put your setup code here, to run once:
   Wire.begin();
   Serial.begin(9600);
   Wire.beginTransmission(devAddr);
+  dataPacket.vel = 0;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
   while (vel > 0) {
     readData();
     calcPosVel();
