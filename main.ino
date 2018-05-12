0d835a566805576dfe282d9efaea2adba967c284
oliverwitham <39222089+oliverwitham@users.noreply.github.com>
2018-05-12 14:47:26 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -2,12 +2,30 @@
 
 const int devAddr = 0x5A;
 const int commBytes = 32;
+DataPacket dataPacket;
+
 
 struct pod {
   long vel;
   long pos;
   long accel;
   int state;
+};
+
+struct DataPacket {
+  byte startingByte;
+  byte errorStates;
+  byte currentPodState;
+  
+  //Where each element is a byte for the 32 bit data.
+  byte timeStamp[4];
+  byte accelFront[4];
+  byte accelMiddle[4];
+  byte accelRear[4];
+  byte tempBraking[4];
+  byte tempPropul[4];
+  byte tempMotherboard[4];
+  byte endingByte;
 };
 
 void calcPosVel() {
@@ -29,6 +47,44 @@ void sendToServer() {
   root["time_since_start"] = dataPacket.time;
   root.printTo(Serial);
 }
+
+for (int i = 0; i < 32; i++) {
+    initDataPacket = parseDataPacket(Wire.read(), i)
+    if(i==0{
+      dataPacket.startingByte = Wire.read()
+    }
+    else if(i==1){
+      dataPacket.errorStates = Wire.read()
+    }
+    else if(i==2){
+      dataPacket.currentPodState = Wire.read()
+    }
+     else if(7 > i > 2){
+      dataPacket.timeStamp[i-3] = Wire.read()
+    }
+    else if(11 > i > 6){
+      dataPacket.accelFront[i-7] = Wire.read()
+    }
+    else if(15 > i > 10){
+      dataPacket.accelMiddle[i-11] = Wire.read()
+    }
+    else if(19 > i > 14){
+      dataPacket.accelRear[i-15] = Wire.read()
+    }
+    else if(23 > i > 18){
+      dataPacket.tempBraking[i-19] = Wire.read()
+    }
+    else if(27 > i > 22){
+      dataPacket.tempPropul[i-23] = Wire.read()
+    }
+    else if(31 > i > 26){
+      dataPacket.tempMotherboard[i-23] = Wire.read()
+    }
+    else{
+      dataPacket.endingByte = Wire.read()
+    }
+}
+       
 
 void setup() {
   // put your setup code here, to run once:
