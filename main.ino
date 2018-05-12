8edfffe1a5a89292d5ee9229f6677a206507713a
oliverwitham <39222089+oliverwitham@users.noreply.github.com>
2018-05-12 14:16:06 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -3,18 +3,25 @@
 const int devAddr = 0x5A;
 const int commBytes = 32;
 
+struct pod {
+  int vel;
+  int pos;
+  int state;
+};
+
 void setup() {
   // put your setup code here, to run once:
   Wire.begin();
   Serial.begin(9600);
   Wire.beginTransmission(devAddr);
-  Wire.requestFrom(devAddr, commBytes);
-  for (int i = 0; i < 32; i++) {
-    Serial.println(Wire.read());
-  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  while (vel > 0) {
+    readData();
+    calcPosVel();
+    sendToServer();
+    controlPod();
+  }
 }
